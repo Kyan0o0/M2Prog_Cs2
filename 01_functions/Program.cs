1d4f71948f8d9a34278d1e375c0abbee3d2e397f
Kyano <40870@ma-web.nl>
2025-11-21 14:57:37 +0100

    a commit

--- 01_functions/Program.cs
+++ 01_functions/Program.cs
@@ -1,9 +1,22 @@
 ﻿namespace _01_functions;
-
-class Program
-{
-    static void Main(string[] args)
+    internal class Program
     {
-        Console.WriteLine("Hello, World!");
+            static void Main(string[] args)
+            {
+                Program program = new Program();
+                program.Run(); // We hebben een variable gemaakt voor de class program. De function die we gebruiken heet Run();. De return type van de function is nu nog niks omdat, de function nog niet aangemaakt is.
+            }
+
+            internal void Run()
+            {
+                Console.WriteLine("...");
+                Vraag1();
+            }
+                internal void Vraag1()
+                {
+                    Console.WriteLine("als deze in je kont zou gaan, Zou je dan nog leven?");
+                    string antwoord = Console.ReadLine();
+
+                    Console.WriteLine(antwoord);
+                }
     }
-}
